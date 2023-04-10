91ea05ddcfa14be4dbb156549fb43e03ccd94cb0
PiLMee <pilmee@Mac-mini-de-PiLMee.local>
2023-04-09 21:37:40 -0500

    feat: add `Example` component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
+
+const Example = lazy(() => import('./Example/Example'));
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>App Example</h1>
       </header>
+      <div>
+        <strong>Example component</strong>
+        <Suspense fallback={<div>Loading</div>}>
+          <Example />
+        </Suspense>
+      </div>
     </div>
   );
 }
---
+++ src/Example/Example.tsx
@@ -0,0 +1,10 @@
+import './Example.scss';
+
+export default function Example() {
+  return (
+    <>
+      <h1 className='title'>Example Component</h1>
+      <p>Example text</p>
+    </>
+  );
+}
